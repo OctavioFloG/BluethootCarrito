fc784d8b3e1e42ad5b8a8b0e1bbdd84bf7171918
Octaviofg <48572130+OctavioFloG@users.noreply.github.com>
2024-10-31 16:03:41 -0600

    no se x2

--- src/main/java/com/example/carritobluethoot/HelloController.java
+++ src/main/java/com/example/carritobluethoot/HelloController.java
@@ -5,10 +5,16 @@ import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onForwardButtonClick() {
     }
+    @FXML
+    protected void onLeftButtonClick() {
+    }
+    @FXML
+    protected void onRightButtonClick() {
+    }
+    @FXML
+    protected void onBackwardButtonClick() {
+    }
+
 }
